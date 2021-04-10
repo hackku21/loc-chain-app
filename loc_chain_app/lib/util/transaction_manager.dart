a7c7133d4f685fec6b9270dec067ec80d09e2769
Zachary Atkins <atkins12@llnl.gov>
2021-04-10 15:26:51 -0500

    implemented hashing

--- loc_chain_app/lib/util/transaction_manager.dart
+++ loc_chain_app/lib/util/transaction_manager.dart
@@ -3,15 +3,25 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:fast_rsa/rsa.dart';
 
+import 'package:loc_chain_app/util/keyfile_manager.dart';
+
 import 'dart:io';
 
 class Transaction {
-  Transaction({required this.otherUserID}) {
+  Transaction({required this.hash}) {
     SharedPreferences.getInstance().then((s) => _id = s.getString('id') ?? '0');
   }
   late final String _id;
-  final String otherUserID;
-  static Hash? makeTransactionHash() {}
+  final String hash;
+  static Future<String> makeTransactionHash(String otherUserId) async {
+    String id = await SharedPreferences.getInstance()
+        .then((s) => s.getString('id') ?? '');
+    bool idLess = id.compareTo(otherUserId) < 0;
+    var lesser = idLess ? id : otherUserId;
+    var greater = idLess ? otherUserId : id;
+    return RSA.signPKCS1v15("$lesser-$greater", Hash.HASH_SHA256,
+        KeyFileManager.keyPair.privateKey);
+  }
 }
 
 class TransactionsDBManager {
