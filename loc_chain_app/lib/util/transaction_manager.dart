8c2dc2a1b78b24a26d9f9e23d5ae86fce6cd63e4
Zachary Atkins <atkins12@llnl.gov>
2021-04-10 23:41:42 -0500

    fix transaction manager

--- loc_chain_app/lib/util/transaction_manager.dart
+++ loc_chain_app/lib/util/transaction_manager.dart
@@ -1,12 +1,5 @@
-import 'package:fast_rsa/model/bridge.pb.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:sqflite/sqflite.dart';
-import 'package:fast_rsa/rsa.dart';
-import 'package:dbcrypt/dbcrypt.dart';
-
+import 'package:flutter_udid/flutter_udid.dart';
 import 'package:loc_chain_app/util/keyfile_manager.dart';
-
-import 'dart:io';
 
 class Transaction {
   Transaction({required this.hash, required this.pubKey});
@@ -14,8 +7,7 @@ class Transaction {
   final String pubKey;
 
   static Future<String> generateHash(String otherUserId) async {
-    String id = await SharedPreferences.getInstance()
-        .then((s) => s.getString('userName') ?? '');
+    String id = await FlutterUdid.consistentUdid;
     bool idLess = id.compareTo(otherUserId) < 0;
     var lesser = idLess ? id : otherUserId;
     var greater = idLess ? otherUserId : id;
@@ -26,14 +18,4 @@ class Transaction {
 
   Future<String> generateP2PPayload(String otherUserId) async =>
       "${await generateHash(otherUserId)}:${KeyFileManager.keyPair.publicKey}";
-}
-
-class TransactionsDBManager {
-  static Future<Database> get _localFile async {
-    return openDatabase('transactions.db');
-  }
-
-  static Future<List<Transaction>?> readTransactions() async {}
-
-  static Future<void> writeKeyPair(KeyPair pair) async {}
 }
